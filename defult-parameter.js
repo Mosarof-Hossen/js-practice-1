cf74cffe6e5b1675aafc7082ef3f6a01af788a82
Mosarof-Hossen <mmd58553@gmail.com>
2025-09-27 09:37:56 +0600

    arrow function & defult perameters learned

---
+++ defult-parameter.js
@@ -0,0 +1,5 @@
+const myPractice= (name="Mosarof",age=23)=>{
+    console.log(`my name is ${name} Hossen .my age is ${age}`)
+    return
+}
+myPractice()
---
+++ practice.js
@@ -0,0 +1,13 @@
+const sum= (a,b)=> a+b;   
+console.log(sum(55,32))
+
+
+function sum1(a,b){
+    return a+b;
+}
+const a=sum1(88,78);
+console.log(a)
+
+
+const squr= a => a*a;
+console.log(squr(10))
