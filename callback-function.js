4c1f4d0e192f01edd59a29ea8eb4b64084db54c2
Mosarof-Hossen <mmd58553@gmail.com>
2025-09-27 14:30:13 +0600

    for ,forEach , map , filter lreand

---
+++ callback-function.js
@@ -0,0 +1,23 @@
+// const greet=(name,greeting,cb)=>{
+//     console.log(`hi, ${name}`)
+//     cb()
+// }
+// const say=(greet)=>{
+//     console
+// }
+
+const arr = [10,21, 53, 63, 23, 63, 76, 86, 32];
+// arr.forEach((ami)=>{
+//     console.log(ami)
+// })
+arr.forEach((ami) => console.log(ami));
+
+// map method // map এর কাজ হলে আপডেট করে দেওয়া যোগ করে দেওয়া
+const m=arr.map((tmi)=> tmi+5); // map এর কাজ হলে আপডেট করে দেওয়া যোগ করে দেওয়া
+console.log(m)
+
+// filter method -filter multiple element
+const adultStudent= arr.filter((ok)=>ok>=18)
+console.log(adultStudent)
+
+// 
